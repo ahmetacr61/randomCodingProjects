053579e73537b028394d7ee5285637cf5c5d8c01
ahmetacr61 <61ahmetacr61@gmail.com>
2023-08-12 12:52:31 +0200

    added first C# file

---
+++ C#/learningCsharp/learningCsharp/learningCsharp.cs
@@ -0,0 +1,43 @@
+﻿//Initializing Variables
+double num1;
+double num2;
+double sol = 0;
+String Operator;
+
+//Query user for Input
+Console.WriteLine("Enter the first Number:");
+num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the operator you want to use");
+Operator = Console.ReadLine();
+Console.WriteLine("Enter the second Number:");
+num2 = double.Parse(Console.ReadLine());
+
+//Choosing the operator to use and calculating
+switch (Operator)
+{
+    case "+":
+        sol = num1 + num2;
+    break;
+    case "-":
+        sol = num1 - num2;
+    break;
+    case "*":
+        sol = num1 * num2;
+    break;
+    case "/":
+        if (num2 != 0)
+        {
+            sol = num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("you can't divide with 0!");
+        }
+    break;
+    
+    default:
+        Console.WriteLine("Something wrent wrong, please restart the code and try again!");
+    break;
+}
+
+Console.WriteLine("Your solution is " + sol);
